f716dd6ec16f3214748946c5c0d2c3e78a734752
MagdalineJ <magdalinegomes@yahoo.com>
2022-04-27 10:33:28 +0100

    creating common funciton

--- js/banking.js
+++ js/banking.js
@@ -1,15 +1,40 @@
-function getInputValue(){
-    const depositInput = document.getElementById('depositAmount');
-    const newdepoAmounttext = depositInput.value;
-    const newdepoAmount=parseFloat(newdepoAmounttext);
+function getInputValue(inputID){
+    const inputField= document.getElementById(inputID);
+    const inputAmounttext = inputField.value;
+    const amountValue=parseFloat(inputAmounttext);
     
      // clear the deposit input field
-     depositInput.value='';
+     inputField.value='';
 
-    return newdepoAmount;
+    return amountValue;
+}
+
+function totalUpdate(totalFieldID, amount){
+    // debugger;
+    const totalAmount=document.getElementById(totalFieldID);
+    const previousTotaltext= totalAmount.innerText;
+    const previousTotal=parseFloat(previousTotaltext)
+    
+    const newTotal= previousTotal+ amount;
+    totalAmount.innerText=newTotal;
+    
+
+}
+
+function updateBalance(amount,isAdd){
+    const balance = document.getElementById('balance-total');
+const balanceTotal= balance.innerText;
+const previousBalance = parseFloat(balanceTotal);
+
+if(isAdd == true){
+    balance.innerText= previousBalance + amount;
+     }
+else{
+    balance.innerText= previousBalance - amount;
 }
 
 
+}
 
 
 
@@ -17,36 +42,41 @@ function getInputValue(){
     document.getElementById('deposit-btn').addEventListener('click',function(){
     // console.log('deposit button clicked')
     // finding the deposit amount
-const newdepoAmount= getInputValue();
+
 
     // const depositInput = document.getElementById('depositAmount');
     // const newdepoAmounttext = depositInput.value;
     // const newdepoAmount=parseFloat(newdepoAmounttext);
-    // console.log(depoAmount);
+    // console.log(newdepoAmount);
 
 
     // getting the current depo value in deposit board
-    const deposittotal=document.getElementById('totaldepo');
+    // const deposittotal=document.getElementById('totaldepo');
     // console.log(deposittotal.innerText);
     // (don't get confused with value and innertext , notice it is in heading tag)
    
-    const previousDepotext= deposittotal.innerText;
-    const previousDepoAmount=parseFloat(previousDepotext)
-    const newDepoTotal= previousDepoAmount + newdepoAmount;
+    // const previousDepotext= deposittotal.innerText;
+    // const previousDepoAmount=parseFloat(previousDepotext)
+    // const newDepoTotal= previousDepoAmount + newdepoAmount;
 
-    deposittotal.innerText=newDepoTotal;
+    // deposittotal.innerText=newDepoTotal;
+
+
 
 // update the total balance on the board
 
-const balance = document.getElementById('balance-total');
-const balanceTotal= balance.innerText;
-const previousBalance = parseFloat(balanceTotal);
-// console.log(previousBalance);
-const currentBalance= previousBalance+ newdepoAmount;
-balance.innerText=currentBalance;
+// const balance = document.getElementById('balance-total');
+// const balanceTotal= balance.innerText;
+// const previousBalance = parseFloat(balanceTotal);
+// // console.log(previousBalance);
+// const currentBalance= previousBalance+ newdepoAmount;
+// balance.innerText=currentBalance;
 
-   
-
+const newdepoAmount= getInputValue('depositAmount');
+if(newdepoAmount>0){
+     totalUpdate('totaldepo',newdepoAmount);  
+    updateBalance(newdepoAmount, true);
+}
 
 })
 
@@ -54,36 +84,39 @@ balance.innerText=currentBalance;
 
 // add event listener
 document.getElementById('withdraw-btn').addEventListener('click',function(){
-    const withdrawInput= document.getElementById('withdrawAmount');
-    const withdrawInputText= withdrawInput.value;
-// console.log(withdrawInputText);
-const withdrawAmount= parseFloat(withdrawInputText);
+//     const withdrawInput= document.getElementById('withdrawAmount');
+//     const withdrawInputText= withdrawInput.value;
+// // console.log(withdrawInputText);
+// const withdrawAmount= parseFloat(withdrawInputText);
+
+
 
 // set withdraw board
-const withdrawTotal = document.getElementById('withdrawal');
-const withdrawalText = withdrawTotal.innerText;
-const previousWithdrawal= parseFloat(withdrawalText);
-const withdrawal = withdrawAmount + previousWithdrawal;
+// const withdrawTotal = document.getElementById('withdrawal');
+// const withdrawalText = withdrawTotal.innerText;
+// const previousWithdrawal= parseFloat(withdrawalText);
+// const withdrawal = withdrawAmount + previousWithdrawal;
 
-withdrawTotal.innerText=withdrawal;
+// withdrawTotal.innerText=withdrawal;
+
+
 
 
 // update total balance
 
-const balance = document.getElementById('balance-total');
-const balanceTotal= balance.innerText;
-const previousBalance = parseFloat(balanceTotal);
-// console.log(previousBalance);
-const currentBalance= previousBalance - withdrawAmount ;
-balance.innerText=currentBalance;
+// const balance = document.getElementById('balance-total');
+// const balanceTotal= balance.innerText;
+// const previousBalance = parseFloat(balanceTotal);
+// // console.log(previousBalance);
+// const currentBalance= previousBalance - withdrawAmount ;
+// balance.innerText=currentBalance;
 
-
-
-// clear withdraw input
-withdrawInput.value='';
-
-
+const withdrawAmount= getInputValue('withdrawAmount');
+if(withdrawAmount>0){
+    totalUpdate('withdrawal',withdrawAmount);
+    updateBalance(withdrawAmount,false);
+    
+}
 })
 
 
-// you have to create  own js files or write on each file in order to let the scripts work
